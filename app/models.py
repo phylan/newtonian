4d6eb2940d5d359023558fe492b65b0787f4390d
Ryan Sommers <ryansommers@gmail.com>
2016-11-17 19:44:33 -0500

    Added skeletal database models

---
+++ app/models.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from . import db
+
+class Document(db.Model):
+	__tablename__ = 'documents'
+	id 
+	fields
+	tags
+	matter
+	messages
+	
+class Field(db.Model):
+	__tablename__ = 'fields'
+	id
+	documents
+	name
+	values
+	type
+	
+class User(db.Model):
+	__tablename__ = 'users'
+	id
+	name
+	handle
+	firm
+	email
+	permission
+	messages
+	matters
+	
+class Tag(db.Model):
+	__tablename__ = 'tags'
+	id
+	name
+	description
+	matter
+	documents
+	
+class Message(db.Model):
+	__tablename__ = 'messages'
+	id
+	matter
+	sender
+	recipients #search string for handles and add?
+	document
+	content
+	timestamp
+	
+
+
+
+	
